babaf775143a1decf47f3a49393bb958c9a02451
Phill-Max <70641709+Phill-Max@users.noreply.github.com>
2020-09-02 17:12:41 +0300

    Add files via upload

---
+++ online-shop/src/Pages/FavItems/FavItems.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import ItemPreview from "../../Components/ItemPreview/ItemPreview";
+import { CardColumns } from "reactstrap";
+
+function FavItemsList(props) {
+  const itemsList = props.location.state;
+  const removeFromFav = props.location.removeFromFav;
+  return (
+    <CardColumns>
+      {itemsList.map((item, index) => (
+        <ItemPreview
+          key={index}
+          price={item.price}
+          name={item.name}
+          url={item.imageUrl}
+          item={item}
+          removeFromFav={removeFromFav}
+        />
+      ))}
+    </CardColumns>
+  );
+}
+
+export default FavItemsList;
