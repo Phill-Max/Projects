61993d70e802913e9e0d4435befee43b87a3808e
Phill-Max <70641709+Phill-Max@users.noreply.github.com>
2020-09-02 16:32:08 +0300

    Add files via upload

---
+++ online-shop/src/Components/ItemPreview/ItemPreview.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+import {
+  Card,
+  Button,
+  CardImg,
+  CardTitle,
+  CardSubtitle,
+  CardBody
+} from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+
+import Modal from "../../Components/Modal/Modal";
+
+import "./ItemPreview.scss";
+
+function ItemPreview(props) {
+  const { name, url, price, removeFromCart, removeFromFav, item } = props;
+  const imgStyle = {
+    maxHeight: 256,
+    maxWidth: 235
+  };
+
+  return (
+    <Card>
+      <CardImg
+        top
+        width="10%"
+        height="10%"
+        src={`${url}`}
+        alt="Card image cap"
+        style={imgStyle}
+      />
+      <CardBody className="d-flex ">
+        <CardTitle>{name}</CardTitle>
+        <CardSubtitle>{price}$</CardSubtitle>
+        <Modal className="btn-md" removeFromCart={removeFromCart} />
+        <Button className="btn-md">
+          <FontAwesomeIcon icon={faStar} onClick={e => removeFromFav(item)} />
+        </Button>
+      </CardBody>
+    </Card>
+  );
+}
+
+export default ItemPreview;
